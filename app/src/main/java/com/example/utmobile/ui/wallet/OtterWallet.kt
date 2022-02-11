25d155859fb1e0b4bdd0ba02e4823db446c3b358
bkronemeyer <bkronemeyer@vmware.com>
2022-02-11 14:59:00 -0600

    Otter wallet formating

--- app/src/main/java/com/example/utmobile/ui/wallet/OtterWallet.kt
+++ app/src/main/java/com/example/utmobile/ui/wallet/OtterWallet.kt
@@ -8,9 +8,11 @@ import kotlin.random.Random
 class OtterWallet(
     var otterBalance: Float,
     val otterId: String = Random.nextInt(999999).toString().format("%06d")) {
+
     fun addOtters(otters: Float) {
         this.otterBalance += otters
     }
+
     fun getBalance() : String {
         val format: NumberFormat = NumberFormat.getCurrencyInstance()
         format.maximumFractionDigits = 2
