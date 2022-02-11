7e466530425cc9ff9e3cd7862dd3835a92962c4d
Jeremy Espinosa <jespinosa@vmare.com>
2022-02-11 14:44:02 -0800

    ASDK-OTTER01 - Weather

--- app/src/main/java/com/example/utmobile/ui/weather/GPSTrackerActivity.java
+++ app/src/main/java/com/example/utmobile/ui/weather/GPSTrackerActivity.java
@@ -1,73 +1,77 @@
 package com.example.utmobile.ui.weather;
 
-import android.content.Intent;
-import android.location.Location;
-import android.os.Bundle;
+import android.os.AsyncTask;
 
-import androidx.appcompat.app.AppCompatActivity;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationServices;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-public class GPSTrackerActivity extends AppCompatActivity implements
-        GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener {
-
-    private GoogleApiClient mGoogleApiClient;
-    Location mLastLocation;
+public class GPSTrackerActivity extends AsyncTask<String, Void, JSONObject> {
+    Exception mException = null;
+    private double longitude = -122.1438405;
+    private double latitude = 37.401245;
+    private String SECRET_API_KEY = "bff97e56087b0a6314c03c2a18392040";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        if (mGoogleApiClient == null) {
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .addApi(LocationServices.API)
-                    .build();
-        }
-
-    }
-
-    protected void onStart() {
-        mGoogleApiClient.connect();
-        super.onStart();
-    }
-
-    protected void onStop() {
-        mGoogleApiClient.disconnect();
-        super.onStop();
+    protected void onPreExecute() {
+        super.onPreExecute();
+        this.mException = null;
     }
 
     @Override
-    public void onConnected(Bundle bundle) {
+    protected JSONObject doInBackground(String... params) {
+        StringBuilder urlString = new StringBuilder();
+        urlString.append("https://api.openweathermap.org/data/2.5/weather?");
+        urlString.append("lat=").append(latitude);
+        urlString.append("&lon=").append(longitude);
+        urlString.append("&units=imperial");
+        urlString.append("&appid=").append(SECRET_API_KEY);
+
+
+        HttpURLConnection urlConnection = null;
+        URL url = null;
+        JSONObject object = null;
+        InputStream inStream = null;
+
+
         try {
-
-            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
-                    mGoogleApiClient);
-            if (mLastLocation != null) {
-                Intent intent = new Intent();
-                intent.putExtra("Longitude", mLastLocation.getLongitude());
-                intent.putExtra("Latitude", mLastLocation.getLatitude());
-                setResult(1,intent);
-                finish();
-
+            url = new URL(urlString.toString());
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setDoInput(true);
+            urlConnection.connect();
+            inStream = urlConnection.getInputStream();
+            BufferedReader bReader = new BufferedReader(new InputStreamReader(inStream));
+            String temp = "";
+            StringBuilder response = new StringBuilder();
+            while ((temp = bReader.readLine()) != null)
+                response.append(temp);
+            bReader.close();
+            inStream.close();
+            urlConnection.disconnect();
+            object = (JSONObject) new JSONTokener(response.toString()).nextValue();
+        } catch (Exception e) {
+            this.mException = e;
+        } finally {
+            if (inStream != null) {
+                try {
+                    // this will close the bReader as well
+                    inStream.close();
+                } catch (IOException ignored) {
+                }
             }
-        } catch (SecurityException e) {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
 
+            return (object);
         }
-
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
 
     }
 }
--- app/src/main/java/com/example/utmobile/ui/weather/WeatherFragment.kt
+++ app/src/main/java/com/example/utmobile/ui/weather/WeatherFragment.kt
@@ -1,77 +1,54 @@
 package com.example.utmobile.ui.weather
 
-import android.Manifest
-import android.app.Instrumentation
-import android.content.Context
-import android.content.Context.LOCATION_SERVICE
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.location.Location
-import android.location.LocationListener
-import android.location.LocationManager
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.utmobile.databinding.FragmentWeatherBinding
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
-import com.google.android.gms.tasks.CancellationToken
-import com.google.android.gms.tasks.CancellationTokenSource
-import java.lang.Exception
+import org.json.JSONObject
 
-class WeatherFragment : Fragment(), LocationListener {
+class WeatherFragment : Fragment() {
 
 private var _binding: FragmentWeatherBinding? = null
-    private lateinit var locationManager: LocationManager
-    private lateinit var locationProvider: FusedLocationProviderClient
-  // This property is only valid between onCreateView and
-  // onDestroyView.
   private val binding get() = _binding!!
+
 
   override fun onCreateView(
     inflater: LayoutInflater,
     container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View {
-    val slideshowViewModel =
-            ViewModelProvider(this).get(WeatherViewModel::class.java)
 
     _binding = FragmentWeatherBinding.inflate(inflater, container, false)
     val root: View = binding.root
 
-    val textView: TextView = binding.textWeather
-    slideshowViewModel.text.observe(viewLifecycleOwner) {
-      textView.text = it
-    }
 
-      val intent = Intent(context, GPSTrackerActivity::class.java)
-      startActivityForResult(intent, 1)
+    getWeather()
 
       return root
   }
 
-    override fun onDestroyView() {
+  @SuppressLint("SetTextI18n")
+  private fun getWeather() {
+
+    val weatherJSON = GPSTrackerActivity().execute("").get()
+
+
+    val main = weatherJSON.get("main") as JSONObject
+
+    binding.tvMinTemp.text = "Low: ${main.get("temp_min")}F"
+    binding.tvMaxTemp.text = "High: ${main.get("temp_max")}F"
+    binding.tvCurrentTemp.text = "Current: ${main.get("temp")}F"
+
+    binding.tvLocation.text = weatherJSON.get("name").toString()
+
+  }
+
+  override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
 
-    override fun onLocationChanged(p0: Location) {
-        TODO("Not implemented yet")
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == 1) {
-            var extras = data!!.extras
-            var longitude = extras!!.getDouble("Longitude")
-            var latitude = extras!!.getDouble("Latitude")
-        }
-    }
 }
